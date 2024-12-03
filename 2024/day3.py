adce3bb73fc6f3d00a550efeb5df444969e49450
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2024-12-03 13:36:30 +0000

    day 3.2

--- 2024/day3.py
+++ 2024/day3.py
@@ -1,2 +1,6 @@
-import re; print(sum([int(mul.split(",")[0].split("(")[-1]
-               ) * int(mul.split(",")[1][:-1]) for mul in re.findall(r"mul\(\d+,\d+\)", "".join(open("day3.txt", "r").read().splitlines()))]))
+import re
+print(f"Part 1: {sum([int(mul.split(",")[0].split("(")[-1]
+                          ) * int(mul.split(",")[1][:-1]) for mul in re.findall(r"mul\(\d+,\d+\)", "".join(open("day3.txt", "r").read().splitlines()))])}")
+do_ = True
+print(f"Part 2: {sum(int(mul.split(",")[0].split("(")[-1]) * int(mul.split(",")[1][:-1]) for mul in re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", "".join(
+    open("day3.txt", "r").read().splitlines())) if (do_ := (mul == "do()") or (do_ if mul != "don't()" else False)) and mul.startswith("mul"))}")
