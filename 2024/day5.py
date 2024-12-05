ab4639a2953e1215b9ef6e74154a12eedcaacca5
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2024-12-05 19:13:52 +0000

    golf ish

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,13 +1,5 @@
 def _is_valid(update):
-    valid = True
-    for rule in rules:
-        a, b = rule.split("|")
-        if a in update and b in update:
-            if update.index(a) > update.index(b):
-                valid = False
-                print(f"Violates {a} | {b}")
-                break
-    return valid
+    return all((a in update and b in update and update.index(a) < update.index(b)) or (a not in update or b not in update) for a, b in (rule.split("|") for rule in rules))
 
 
 with open("day5.txt", "r")as f:
@@ -17,11 +9,8 @@ rules = [d.split("\n") for d in data][0]
 updates = [d.split("\n") for d in data][1][:-1]
 updates = [d.split(",") for d in updates]
 
-answer = 0
-for update in updates:
-    if _is_valid(update):
-        answer += int(update[len(update)//2])
-print(answer)
+print(sum((int(update[len(update)//2])
+      if _is_valid(update) else 0 for update in updates)))
 
 answer = 0
 for update in updates:
