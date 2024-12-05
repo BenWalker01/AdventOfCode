a6a991307aeca6777c43fa29a1471bb9a3eef73a
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2024-12-05 14:39:23 +0000

    remove brain

--- 2024/day5.py
+++ 2024/day5.py
@@ -10,25 +10,6 @@ def _is_valid(update):
     return valid
 
 
-def _generate_valid(update):
-    valid_list = []
-    for rule in rules:
-        a, b = rule.split("|")
-        if a in update and b in update:
-            if a in valid_list and b in valid_list:
-                if valid_list.index(a) > valid_list.index(b):
-                    valid_list[valid_list.index(a)], valid_list[valid_list.index(
-                        b)] = valid_list[valid_list.index(b)], valid_list[valid_list.index(a)]
-            elif a in valid_list:
-                valid_list.insert(valid_list.index(a) + 1, b)
-            elif b in valid_list:
-                valid_list.insert(valid_list.index(b), a)
-            else:
-                valid_list.append(a)
-                valid_list.append(b)
-    return valid_list
-
-
 with open("day5.txt", "r")as f:
     data = f.read().split("\n\n")
 
