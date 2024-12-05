d40c5b95f33539dd32183ba7916df06e1001ecf9
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2024-12-05 14:38:43 +0000

    day5.2

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,3 +1,34 @@
+def _is_valid(update):
+    valid = True
+    for rule in rules:
+        a, b = rule.split("|")
+        if a in update and b in update:
+            if update.index(a) > update.index(b):
+                valid = False
+                print(f"Violates {a} | {b}")
+                break
+    return valid
+
+
+def _generate_valid(update):
+    valid_list = []
+    for rule in rules:
+        a, b = rule.split("|")
+        if a in update and b in update:
+            if a in valid_list and b in valid_list:
+                if valid_list.index(a) > valid_list.index(b):
+                    valid_list[valid_list.index(a)], valid_list[valid_list.index(
+                        b)] = valid_list[valid_list.index(b)], valid_list[valid_list.index(a)]
+            elif a in valid_list:
+                valid_list.insert(valid_list.index(a) + 1, b)
+            elif b in valid_list:
+                valid_list.insert(valid_list.index(b), a)
+            else:
+                valid_list.append(a)
+                valid_list.append(b)
+    return valid_list
+
+
 with open("day5.txt", "r")as f:
     data = f.read().split("\n\n")
 
@@ -7,13 +38,19 @@ updates = [d.split(",") for d in updates]
 
 answer = 0
 for update in updates:
-    valid = True
-    for rule in rules:
-        a, b = rule.split("|")
-        if a in update and b in update:
-            if update.index(a) > update.index(b):
-                valid = False
-                break
-    if valid:
+    if _is_valid(update):
         answer += int(update[len(update)//2])
+print(answer)
+
+answer = 0
+for update in updates:
+    if not _is_valid(update):
+        while not _is_valid(update):
+            for rule in rules:
+                a, b = rule.split("|")
+                if a in update and b in update:
+                    update[update.index(a)], update[update.index(
+                        b)] = update[update.index(b)], update[update.index(a)]
+        answer += int(update[len(update)//2])
+
 print(answer)
